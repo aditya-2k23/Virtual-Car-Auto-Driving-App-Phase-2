6ed4f82e0741e0666ec7727aa4d06719ba810063
Aditya <holaaditya123@gmail.com>
2024-04-20 18:27:29 +0530

    Add treeSize property, and now the trees don't intersect with the roads and the buildings given it's size

--- js/world.js
+++ js/world.js
@@ -5,7 +5,8 @@ class World {
     roadRoundness = 10,
     buildingWidth = 150,
     buildingMinLength = 150,
-    spacing = 50
+    spacing = 50,
+    treeSize = 160
   ) {
     this.graph = graph;
     this.roadWidth = roadWidth;
@@ -13,6 +14,7 @@ class World {
     this.buildingWidth = buildingWidth;
     this.buildingMinLength = buildingMinLength;
     this.spacing = spacing;
+    this.treeSize = treeSize;
 
     this.envelopes = [];
     this.roadBorders = [];
@@ -58,12 +60,21 @@ class World {
       );
 
       let keep = true;
-      for (const poly of illegalPolys) {
-        if (poly.containsPoint(p)) {
+      for (const poly of illegalPolys)
+        if (
+          poly.containsPoint(p) ||
+          poly.distanceToPoint(p) < this.treeSize / 2
+        ) {
           keep = false;
           break;
         }
-      }
+
+      if (keep)
+        for (const tree of trees)
+          if (distance(tree, p) < this.treeSize) {
+            keep = false;
+            break;
+          }
 
       if (keep) trees.push(p);
     }
@@ -142,7 +153,7 @@ class World {
       seg.draw(ctx, { color: "white", width: 4 });
     }
     for (const tree of this.trees) {
-      tree.draw(ctx);
+      tree.draw(ctx, { size: this.treeSize, color: "rgba(0,0,0,0.5)" });
     }
     for (const bld of this.buildings) {
       bld.draw(ctx);
