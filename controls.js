3863b0da273fb054eb668c89c8d0a794020cdcf6
Aditya <holaaditya123@gmail.com>
2024-06-10 17:31:50 +0530

    Merging the Phase I code into Phase II

---
+++ controls.js
@@ -0,0 +1,52 @@
+class Controls {
+  constructor(type) {
+    this.forward = false;
+    this.left = false;
+    this.right = false;
+    this.reverse = false;
+
+    switch (type) {
+      case "KEYS":
+        this.#addKeyboardListeners();
+        break;
+      case "DUMMY":
+        this.forward = true;
+        break;
+    }
+  }
+
+  #addKeyboardListeners() {
+    document.onkeydown = (event) => {
+      switch (event.key) {
+        case "ArrowLeft" || "a":
+          this.left = true;
+          break;
+        case "ArrowRight" || "d":
+          this.right = true;
+          break;
+        case "ArrowUp" || "w":
+          this.forward = true;
+          break;
+        case "ArrowDown" || "s":
+          this.reverse = true;
+          break;
+      }
+    };
+    document.onkeyup = (event) => {
+      switch (event.key) {
+        case "ArrowLeft":
+          this.left = false;
+          break;
+        case "ArrowRight":
+          this.right = false;
+          break;
+        case "ArrowUp":
+          this.forward = false;
+          break;
+        case "ArrowDown":
+          this.reverse = false;
+          break;
+      }
+    };
+  }
+}
